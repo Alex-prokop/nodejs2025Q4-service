fda532bfbc1f6e43417d6970fa9112605ca01f28
Alexandr Prokopovich <prokop7@mail.ru>
2025-11-25 17:43:32 +0300

    chore: configure env port and global validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
@@ -10,6 +11,10 @@ import { FavoritesModule } from './modules/favorites/favorites.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     DatabaseModule,
     UserModule,
     ArtistModule,
