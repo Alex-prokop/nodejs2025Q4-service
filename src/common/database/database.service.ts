8203ef078090ef7efcb85492d459937b291eb02a
Alexandr Prokopovich <prokop7@mail.ru>
2025-11-26 08:22:53 +0300

    feat: implement tracks module with basic CRUD

--- src/common/database/database.service.ts
+++ src/common/database/database.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { User } from '../../modules/user/entities/user.entity';
 import { Artist } from '../../modules/artist/entities/artist.entity';
 import { Album } from '../../modules/album/entities/album.entity';
+import { Track } from '../../modules/track/entities/track.entity';
 
 @Injectable()
 export class DatabaseService {
   users: User[] = [];
   artists: Artist[] = [];
   albums: Album[] = [];
-  tracks: unknown[] = [];
+  tracks: Track[] = [];
 
   favorites = {
     artists: [] as string[],
---
+++ src/modules/track/track.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { DatabaseService } from '../../common/database/database.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track, TrackResponse } from './entities/track.entity';
+
+@Injectable()
+export class TrackService {
+  constructor(private readonly db: DatabaseService) {}
+
+  findAll(): TrackResponse[] {
+    return this.db.tracks;
+  }
+
+  findOne(id: string): TrackResponse {
+    const track = this.db.tracks.find((t) => t.id === id);
+
+    if (!track) {
+      throw new NotFoundException('Track not found');
+    }
+
+    return track;
+  }
+
+  create(dto: CreateTrackDto): TrackResponse {
+    const track: Track = {
+      id: randomUUID(),
+      name: dto.name,
+      artistId: dto.artistId ?? null,
+      albumId: dto.albumId ?? null,
+      duration: dto.duration,
+    };
+
+    this.db.tracks.push(track);
+
+    return track;
+  }
+
+  update(id: string, dto: UpdateTrackDto): TrackResponse {
+    const track = this.db.tracks.find((t) => t.id === id);
+
+    if (!track) {
+      throw new NotFoundException('Track not found');
+    }
+
+    if (dto.name !== undefined) {
+      track.name = dto.name;
+    }
+
+    if (dto.artistId !== undefined) {
+      track.artistId = dto.artistId;
+    }
+
+    if (dto.albumId !== undefined) {
+      track.albumId = dto.albumId;
+    }
+
+    if (dto.duration !== undefined) {
+      track.duration = dto.duration;
+    }
+
+    return track;
+  }
+
+  remove(id: string): void {
+    const index = this.db.tracks.findIndex((t) => t.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Track not found');
+    }
+
+    // TODO!!!
+    this.db.tracks.splice(index, 1);
+  }
+}
